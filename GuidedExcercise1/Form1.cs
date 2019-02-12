0b56e55839ec9acf63eb978fcc06a20909d21817
Jordon Cotton <s188044@students.aie.edu>
2019-02-11 18:01:23 -0600

    Add project files.

---
+++ GuidedExcercise1/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuidedExcercise1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs window)
+        {
+            System.IO.File(Button1_Click);
+        }
+    }
+}
